69c2c67bfdfb76eb16e4fda85f1dbd464ed87b42
Bijay <bijay.pokhrel05@gmail.com>
2023-06-30 09:07:37 +0545

    Added factorial code

---
+++ factorial.py
@@ -0,0 +1,13 @@
+def fact(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fact(n-1) * n
+
+n = 0
+try:
+    n = int(input("Enter a number:"))
+except ValueError:
+    print("Invalid number. You must have to insert the integer value only!")
+y = fact(n)
+print(f"The factorial of {n} is {y}.")
